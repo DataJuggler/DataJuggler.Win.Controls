ba7bbb9955222b1037bf18b4dafe55c8a155500d
Corby <corby@datajuggler.com>
2019-06-03 08:03:05 -0500

    This is the first commit now that DataJuggler.Win.Controls is in its own repo.

---
+++ DataJuggler.Win.Controls/Enumerations/ClientEnumerations.cs
@@ -0,0 +1,39 @@
+﻿
+
+#region using statements
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#endregion
+
+namespace DataJuggler.Win.Controls.Enumerations
+{
+
+    #region BrowseTypeEnum : int 
+    /// <summary>
+    /// This enumeration is used to determine which type of an object is being browsed for
+    /// </summary>
+    public enum BrowseTypeEnum : int 
+    {
+        File = 0,
+        Folder = 1,
+        CustomOpen = 2
+    }
+    #endregion
+
+    #region ThemeEnum
+    /// <summary>
+    /// This enumeration is for the type of theme to use.
+    /// For now there are only two choices, Blue and Dark.
+    /// </summary>
+    public enum ThemeEnum : int
+    {
+        Dark = 0,
+        Blue = 1
+    }
+    #endregion
+
+}
