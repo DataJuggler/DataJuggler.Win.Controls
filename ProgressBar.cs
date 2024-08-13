e33fb17b04801b0e6e6dedb827ab6d9d32f4b502
DataJuggler <corby@datajuggler.com>
2024-08-13 18:05:48 -0500

    DataJuggler.UltimateHelper was updated.

---
+++ ProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataJuggler.Win.Controls
+{
+    public partial class ProgressBar : UserControl
+    {
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
